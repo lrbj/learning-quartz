c71c5d4278e982c86c16e7cae4c2f44558ac7492
H299633 <shuzhen.ye@honeywell.com>
2019-01-28 16:54:13 +0800

    数据持久化

--- quartz/src/main/java/com/example/demo/task/QuartzTask1.java
+++ quartz/src/main/java/com/example/demo/task/QuartzTask1.java
@@ -1,5 +1,8 @@
 package com.example.demo.task;
 
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -10,8 +13,9 @@ import java.util.Date;
  * @Date:Created in 3:46 PM 1/24/2019
  */
 @Service
-public class QuartzTask1 {
-    public  void pring(){
+public class QuartzTask1 implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("执行业务处理task1：" + new Date());
     }
 }
--- quartz/src/test/java/com/example/demo/DemoApplicationTests.java
+++ quartz/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.task.QuartzTask1;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,46 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+
+    @Test
+    public void test(){
+        try {
+        //1、创建scheduler的工厂
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+
+        //2、从工厂中获取调度器实例
+            Scheduler scheduler = schedulerFactory.getScheduler();
+
+         //3、创建JobDetail
+            JobDetail  jobDetail = JobBuilder.newJob( QuartzTask1.class)
+                    .withDescription("测试的定时任务。")//job的描述
+                    .withIdentity("test1", "test1group")//任务job和name 和group
+                    .build();
+
+            //任务运行的时间
+            long time = System.currentTimeMillis() + 2*1000L;//3秒后启动
+
+            Date statime = new Date(time);
+
+            //4、创建Trigger
+            Trigger t = TriggerBuilder.newTrigger()
+                    .withDescription("")
+                    .withIdentity("test1","test1group")
+                    .startAt(statime)
+                    .withSchedule(CronScheduleBuilder.cronSchedule("0/2 * * * * ?"))
+                    .build();//每两秒执行一次
+
+            //5、注册任务和定时器
+            scheduler.scheduleJob(jobDetail, t);
+
+            //6、启动调度器
+            scheduler.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
