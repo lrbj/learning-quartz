9e07444e1c9da3ffbd2eed0a37962ba7aed79445
H299633 <shuzhen.ye@honeywell.com>
2019-04-26 16:54:24 +0800

    注释之后代码可正常运行

--- quartz/src/main/java/com/example/demo/task/QuartzTask1.java
+++ quartz/src/main/java/com/example/demo/task/QuartzTask1.java
@@ -1,21 +1,21 @@
-package com.example.demo.task;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
-
-/**
- * @Author: Kayla, Ye
- * @Description:
- * @Date:Created in 3:46 PM 1/24/2019
- */
-@Service
-public class QuartzTask1 implements Job {
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("执行业务处理task1：" + new Date());
-    }
-}
+//package com.example.demo.task;
+//
+//import org.quartz.Job;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Date;
+//
+///**
+// * @Author: Kayla, Ye
+// * @Description:
+// * @Date:Created in 3:46 PM 1/24/2019
+// */
+//@Service
+//public class QuartzTask1 implements Job {
+//    @Override
+//    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        System.out.println("执行业务处理task1：" + new Date());
+//    }
+//}
